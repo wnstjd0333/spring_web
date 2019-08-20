636e4a4f7094b005f54274a205e43d19d95dbebc
user <user@user-PC>
2019-08-20 10:02:02 +0900

    5-3

---
+++ 5-3/src/main/java/controller/LoginFormController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import logic.Shop;
+import model.User;
+
+@Controller
+public class LoginFormController {
+	private Shop shopService;
+	private Validator loginValidator;
+	
+	public void setShopService(Shop shopService) {
+		this.shopService = shopService;
+	}
+	public void setLoginValidator(Validator loginValidator) {
+		this.loginValidator = loginValidator;
+	}
+	
+	@ModelAttribute
+	public User setup() {
+		return new User();
+	}//콜백메서드(user생성)
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String setupForm() {//콜백메서드
+		return "login";
+	}//콜백 메서드(login뷰 호출)
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView onSubmit(User user,BindingResult br) { //ModelAndView <- 결과, 뷰
+		this.loginValidator.validate(user, br);
+		ModelAndView mav = new ModelAndView();
+		if(br.hasErrors()) {//유효성 검사 결과가 에러가 있는 경우
+			mav.getModel().putAll(br.getModel());
+			//로그인 화면에서 입력했던 데이터를 지우지 않고 다시 출력한다.
+			return mav;
+		}
+		try {
+			User loginUser=this.shopService.getUser(user);
+			if(loginUser != null) {//로그인 성공한 경우
+				mav.setViewName("loginSuccess");
+				mav.addObject("loginUser",loginUser);
+				return mav;
+			} else { //로그인 실패한 경우
+				br.reject("error.login.user");//화면에 출력될 메세지 설정
+				mav.getModel().putAll(br.getModel());
+				return mav;
+			}
+		}catch(EmptyResultDataAccessException e) {
+			br.reject("error.login.user");
+			mav.getModel().putAll(br.getModel());
+			return mav;
+		}
+	}
+}
